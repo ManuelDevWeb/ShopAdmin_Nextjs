2b24fb5b45af147e8637371c33d7d2be1f4ddc22
ManuelDev <manueldeveloper17@gmail.com>
2023-10-12 02:36:07 -0500

    Feature: add common components and main layout

---
+++ src/layout/MainLayout.js
@@ -0,0 +1,19 @@
+// Components
+import Header from '@components/Header';
+import Nav from '@common/Nav';
+
+const MainLayout = ({ children }) => {
+  return (
+    <>
+      <div className="min-h-full">
+        <Header />
+        <Nav />
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export { MainLayout };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
-const colors = require('tailwindcss/colors');
+let colors = require('tailwindcss/colors');
+
+delete colors.lightBlue;
+delete colors.warmGray;
+delete colors.trueGray;
+delete colors.blueGray;
+delete colors.coolGray;
 
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
